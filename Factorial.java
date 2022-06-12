1c4e1fd26536925e15f33b7b5effb52f722a9c6b
Lavanya2281 <107319249+Lavanya2281@users.noreply.github.com>
2022-06-12 17:55:02 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+package week1.day1.assignments;
+
+public class Factorial {
+	//Goal: Find the Factorial of a given number
+public static void main(String[] args) {
+	int input = 5;
+	int fact = 1;
+	for (int i = 1;i<=input;i++) {
+		fact = fact * i;
+	}
+	System.out.println(fact);
+}
+}
